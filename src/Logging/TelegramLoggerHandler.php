fd178affd6629c8877cbbd90426556cb17f1886e
anbozsultonov <anboz98sultonov@gmail.com>
2022-11-05 14:30:04 +0500

    create telegram logger

---
+++ src/Logging/TelegramLoggerHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tglogger\Logchannel\Logging;
+
+use Tglogger\Logchannel\Services\Telegram\TelegramBotApi;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+final class TelegramLoggerHandler extends AbstractProcessingHandler
+{
+    protected int $chatId;
+
+    protected string $token;
+
+    public function __construct(array $config)
+    {
+        $this->chatId = $config['chat_id'];
+        $this->token = $config['token'];
+
+        $level = Logger::toMonologLevel($config['level']);
+
+        parent::__construct($level);
+    }
+
+    public function write(array $record): void
+    {
+        TelegramBotApi::sendMessage(
+            $this->token,
+            $this->chatId,
+            $record['formatted']
+        );
+    }
+}
