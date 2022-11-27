35747bce54d1dad41e4bfd7dd552d8a6bfc408d8
anbozsultonov <anboz98sultonov@gmail.com>
2022-11-27 11:28:20 +0500

    catch exception

--- src/Services/Telegram/TelegramBotApi.php
+++ src/Services/Telegram/TelegramBotApi.php
@@ -3,6 +3,7 @@
 namespace Tglogger\Logchannel\Services\Telegram;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 final class TelegramBotApi
 {
@@ -10,12 +11,18 @@ final class TelegramBotApi
 
     public static function sendMessage(string $token, int $chatId, string $text): void
     {
-        Http::get(
-            self::HOST . $token . '/sendMessage',
-            [
-                'chat_id' => $chatId,
-                'text' => $text
-            ]
-        );
+        try {
+            Http::get(
+                self::HOST . $token . '/sendMessage',
+                [
+                    'chat_id' => $chatId,
+                    'text' => $text
+                ]
+            );
+        } catch (\Exception $e) {
+            Log::error(json_encode([
+                'telegram' => $e->getMessage()
+            ], JSON_THROW_ON_ERROR));
+        }
     }
 }
